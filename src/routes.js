8d3dceb73cb05f6944f617e3a7f50828b87addda
Nicolas Karlezi <nicolaskarlezi@MacBook-Pro-de-Nicolas.local>
2021-11-05 16:46:23 -0300

    mover productos almacenes

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,10 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 const almacenes = require('./routes/almacenes');
-const moverStock = require('./routes/moverStock');
+const fabricarSinPago = require('./routes/fabricarSinPago');
 const skuWithStock = require('./routes/skuWithStock');
 const stockalmacen = require('./routes/stockalmacen');
+const moverstock = require('./routes/moverstock');
 
 
 const router = new KoaRouter();
@@ -13,9 +14,10 @@ const router = new KoaRouter();
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/almacenes', almacenes.routes());
-router.use('/moverStock', moverStock.routes());
+router.use('/fabricarSinPago', fabricarSinPago.routes());
 router.use('/skuWithStock',skuWithStock.routes());
 router.use('/stockalmacen', stockalmacen.routes());
+router.use('/moverstock', moverstock.routes());
 
 
 module.exports = router;
--- src/routes/fabricarSinPago.js
+++ src/routes/fabricarSinPago.js
@@ -7,27 +7,32 @@ const router = new KoaRouter();
 const key = "O8vKmNc.6XL0K1";
 
 
-router.post('moverstock', '/', async (ctx) => {
+router.put('fabricar', '/', async (ctx) => {
   try {
     const body = ctx.request.body;
-    const productoId = body.productoId;
-    const almacenId = body.almacenId;
+    const sku = body.sku;
+    const cantidad = body.cantidad;
     hmac = crypto.createHmac("sha1", key);
-    hmac.update("POST"+String(productoId)+String(almacenId));
+    hmac.update("PUT"+String(sku)+String(cantidad));
     hash = hmac.digest('base64');
-    console.log("POST: ", "INTEGRACION grupo12:"+String(hash));
-    const response = await fetch('https://dev.api-bodega.2021-2.tallerdeintegracion.cl/bodega/moveStock', {
-      method: "POST",
+    console.log("Method 4: ", "INTEGRACION grupo12:"+String(hash));
+    console.log(sku);
+    console.log(cantidad);
+    console.log(body);
+    const response = await fetch('https://dev.api-bodega.2021-2.tallerdeintegracion.cl/bodega/fabrica/fabricarSinPago', {
+      method: "PUT",
       headers: { "Content-Type": "application/json",
                "Authorization": "INTEGRACION grupo12:"+String(hash)
               },
-      body: JSON.stringify({ "productoId": productoId,
-              "almacenId": almacenId })
-    });
+      body :JSON.stringify({ "sku": sku,
+              "cantidad": cantidad
+            })
+      });
+    
     const responsee = await response.json();
     console.log(responsee);
-    }
-    catch (ValidationError) {
+  }
+  catch (ValidationError) {
     ctx.throw(400);
   }
 });
